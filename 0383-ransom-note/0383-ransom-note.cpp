be917e6ad223ea16ea98e7da12cf390dae293ed2
Arif Sheikh <arifsheikh0018@gmail.com>
2026-08-02 17:29:56 +0530

    Time: 0 ms (100%), Space: 11.9 MB (49.65%) - LeetHub

---
+++ 0383-ransom-note/0383-ransom-note.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool canConstruct(string ransomNote, string magazine) {
+        unordered_map<char, int> count;
+        for (int i = 0; i < magazine.length(); i++){
+            count[magazine[i]]++;
+        }
+        for (int i = 0; i < ransomNote.length(); i++){
+            if (count[ransomNote[i]] == 0){
+                return false;
+            } else {
+                count[ransomNote[i]]--;
+            }
+        }
+        return true;
+    }
+};
